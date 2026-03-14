2a434ab27db08e9cdd3f7423108c83fdd0fb524f
locnguyenlaptrinh <locn0920@gmail.com>
2026-03-14 21:44:38 +0700

    Update and rename Kiểm tra năm nhuận to Kiểm tra năm nhuận.C

--- Kiểm tra năm nhuận.C
+++ Kiểm tra năm nhuận.C
@@ -1,6 +1,7 @@
 CHO AI CẦN THAM KHẢO🥰
 Năm nhuận là năm chia hết cho 400 hoặc (chia hết cho 4 và không chia hết cho 100). Nhập vào N là một năm và kiểm tra xem N có phải là năm nhuận hay không?
- #include<stdio.h>
+
+#include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 int main(){
